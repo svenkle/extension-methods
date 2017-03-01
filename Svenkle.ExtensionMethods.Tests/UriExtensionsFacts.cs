2f4c98a08f184bc5e1c71b38ecf40c848e661b43
Luke Arentz <luke.arentz@dt.com.au>
2017-03-01 13:44:27 +1100

    Created URI extensions
    Created new class and unit tests for GetBasePath extension method

---
+++ Svenkle.ExtensionMethods.Tests/UriExtensionsFacts.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Xunit;
+
+namespace Svenkle.ExtensionMethods.Tests
+{
+    public class UriExtensionsFacts
+    {
+        public class TheGetBaseUriMethod
+        {
+            private const string QueryString = "?myquerystring=1";
+            private const string Path = "/myvalue/";
+            private const string Port = "8080";
+            private const string Protocol = "http://";
+            private static readonly Uri Uri = new Uri($"{Protocol}www.google.com:{Port}/{Path}{QueryString}");
+
+
+            [Fact]
+            public void ReturnsStringWithProtocol()
+            {
+                // Prepare & Act
+                var urlString = Uri.GetBaseUri();
+
+                // Assert
+                Assert.StartsWith(Protocol, urlString);
+            }
+
+            [Fact]
+            public void ReturnsStringWithPort()
+            {
+                // Prepare & Act
+                var urlString = Uri.GetBaseUri();
+
+                // Assert
+                Assert.Contains(Port, urlString);
+            }
+
+            [Fact]
+            public void ReturnsStringWithoutPath()
+            {
+                // Prepare & Act
+                var urlString = Uri.GetBaseUri();
+
+                // Assert
+                Assert.DoesNotContain(Path, urlString);
+            }
+
+            [Fact]
+            public void ReturnsStringWithoutQueryString()
+            {
+                // Prepare & Act
+                var urlString = Uri.GetBaseUri();
+
+                // Assert
+                Assert.DoesNotContain(QueryString, urlString);
+            }
+
+            [Fact]
+            public void ReturnsStringWithTrailingSlash()
+            {
+                // Prepare & Act
+                var urlString = Uri.GetBaseUri();
+
+                // Assert
+                Assert.False(urlString.EndsWith("/"));
+            }
+        }
+    }
+}
